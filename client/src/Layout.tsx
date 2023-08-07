9ea76db2758964bc663af64a985d02900f20fa0a
Ahmed <imaneben8@gmail.com>
2023-08-07 20:18:06 +0100

    feat: implemented the layout

---
+++ client/src/Layout.tsx
@@ -0,0 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+
+import { Link } from "react-router-dom";
+import Confessions from "./components/ Confessions";
+import Misdemeanour from "./components/Misdemeanour";
+import { NotFound } from "./components/Not_found";
+
+export const Layout = () => {
+  <>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/misdemeanour">Misdemeanour</Link>
+      </li>
+      <li>
+        <Link to="confessions">Confessions</Link>
+      </li>
+    </ul>
+    <Routes>
+      <Route path="/">
+        <Route path="confessions" element={<Confessions />} />
+        <Route path="misdemeanour" element={<Misdemeanour />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  </>;
+};
