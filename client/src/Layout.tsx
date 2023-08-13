7ef123408f0fccb3e044be1daa2dd4a924a80a30
Ahmed <imaneben8@gmail.com>
2023-08-14 00:18:48 +0100

    feat: implemented home page

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -1,6 +1,7 @@
 import Confessions from "./components/ Confessions";
 import { Misdemeanour } from "./components/Misdemeanour";
 import { NotFound } from "./components/Not_Found";
+import Home from "./components/home";
 import { NavLink } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 
@@ -8,17 +9,18 @@ export const Layout: React.FC = () => (
   <>
     <ul>
       <li>
-        <NavLink to="/">Home</NavLink>
+        <NavLink to="/home">Home</NavLink>
       </li>
       <li>
         <NavLink to="/misdemeanour">Misdemeanour</NavLink>
       </li>
       <li>
-        <NavLink to="confessions">Confessions</NavLink>
+        <NavLink to="confessions">Confess To Us</NavLink>
       </li>
     </ul>
     <Routes>
       <Route path="/">
+        <Route path="home" element={<Home />} />
         <Route path="confessions" element={<Confessions />} />
         <Route path="misdemeanour" element={<Misdemeanour />} />
         <Route path="*" element={<NotFound />} />
--- client/src/components/home.tsx
+++ client/src/components/home.tsx
@@ -1,3 +1,22 @@
-const Home: React.FC = () => <>Home!</>;
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-purple-100">
+      <p className="text-center text-2xl">
+        <strong>
+          Welcome to the home of the Justice Department of Fakelandia!
+        </strong>
+      </p>
+      <br />
+      <p className="text-center text-2xl">
+        <strong>
+          Here you can browse a list of recent misdemeanours committed by our
+          citizens, or you can confess to your own misdemeanour.
+        </strong>
+      </p>
+    </div>
+  );
+};
 
 export default Home;
