4f59e57be3e06e02cc4da231d626efbc83207a30
Ahmed <imaneben8@gmail.com>
2023-08-14 00:34:55 +0100

    feat: added img to the home page

--- client/src/components/home.tsx
+++ client/src/components/home.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import Justice from "../assets/Justice.png";
 
 const Home: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-purple-100">
+    <div
+      className="flex flex-col items-center justify-center min-h-screen bg-purple-100"
+      style={{
+        backgroundImage: `url(${Justice})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
       <p className="text-center text-2xl">
         <strong>
           Welcome to the home of the Justice Department of Fakelandia!
